a8d10438ca40c9d63c77ae9e647f2683de212da8
lkaprielian <lisakaprielian.lk@gmail.com>
2024-04-12 11:22:59 +0200

    Update module.reports.availreport.js.php

--- views/js/module.reports.availreport.js.php
+++ views/js/module.reports.availreport.js.php
@@ -5,178 +5,186 @@
  */
 ?>
 <script type="text/javascript">
-	jQuery(function($) {
-		function availreportPage() {
-			let filter_options = <?= json_encode($data['filter_options']) ?>;
-			this.refresh_url = '<?= $data['refresh_url'] ?>';
-			this.refresh_interval = <?= $data['refresh_interval'] ?>;
-			this.running = false;
-			this.timeout = null;
-			this.deferred = null;
+    jQuery(function($) {
+        function availreportPage() {
+            let filter_options = <?= json_encode($data['filter_options']) ?>;
+            this.refresh_url = '<?= $data['refresh_url'] ?>';
+            this.refresh_interval = <?= $data['refresh_interval'] ?>;
+            this.running = false;
+            this.timeout = null;
+            this.deferred = null;
+            this.timeselector_data = null; // Add a property to store timeselector data
 
-			if (filter_options) {
-				this.refresh_counters = this.createCountersRefresh(1);
-				this.filter = new CTabFilter($('#reports_availreport_filter')[0], filter_options);
-				this.filter.on(TABFILTER_EVENT_URLSET, (ev) => {
-					let url = new Curl('', false);
+            if (filter_options) {
+                this.refresh_counters = this.createCountersRefresh(1);
+                this.filter = new CTabFilter($('#reports_availreport_filter')[0], filter_options);
+                this.filter.on(TABFILTER_EVENT_URLSET, (ev) => {
+                    let url = new Curl('', false);
 
-					url.setArgument('action', 'availreport.view.refresh');
-					this.refresh_url = url.getUrl();
-					this.unscheduleRefresh();
-					this.refresh();
+                    url.setArgument('action', 'availreport.view.refresh');
+                    this.refresh_url = url.getUrl();
+                    this.unscheduleRefresh();
+                    this.refresh();
 
-					var filter_item = this.filter._active_item;
+                    var filter_item = this.filter._active_item;
 
-					if (this.filter._active_item.hasCounter()) {
-						$.post('zabbix.php', {
-							action: 'availreport.view.refresh',
-							filter_counters: 1,
-							counter_index: filter_item._index
-						}).done((json) => {
-							if (json.filter_counters) {
-								filter_item.updateCounter(json.filter_counters.pop());
-							}
-						});
-					}
-				});
-			}
-		}
+                    if (this.filter._active_item.hasCounter()) {
+                        $.post('zabbix.php', {
+                            action: 'availreport.view.refresh',
+                            filter_counters: 1,
+                            counter_index: filter_item._index
+                        }).done((json) => {
+                            if (json.filter_counters) {
+                                filter_item.updateCounter(json.filter_counters.pop());
+                            }
+                        });
+                    }
+                });
+            }
 
-		availreportPage.prototype = {
-			createCountersRefresh: function(timeout) {
-				if (this.refresh_counters) {
-					clearTimeout(this.refresh_counters);
-					this.refresh_counters = null;
-				}
+            // Save timeselector data when filter button is clicked
+            $('#your_filter_button_id').on('click', () => {
+                if (window.availreport_page && window.availreport_page.timeselector_data) {
+                    window.availreport_page.timeselector_data = { ...window.availreport_page.timeselector_data };
+                }
+            });
+        }
 
-				return setTimeout(() => this.getFiltersCounters(), timeout);
-			},
-			getFiltersCounters: function() {
-				return $.post('zabbix.php', {
-						action: 'availreport.view.refresh',
-						filter_counters: 1
-					}).done((json) => {
-						if (json.filter_counters) {
-							this.filter.updateCounters(json.filter_counters);
-						}
-					}).always(() => {
-						if (this.refresh_interval > 0) {
-							this.refresh_counters = this.createCountersRefresh(this.refresh_interval);
-						}
-					});
-			},
-			getCurrentForm: function() {
-				return $('form[name=availreport_view]');
-			},
-			addMessages: function(messages) {
-				$('.wrapper main').before(messages);
-			},
-			removeMessages: function() {
-				$('.wrapper .msg-bad').remove();
-			},
-			refresh: function() {
-				// Update export_csv url according to what's in filter fields
-				const export_csv_url = new URL(this.refresh_url, 'http://example.com');
-				for(var key of export_csv_url.searchParams.keys()) {
-					if (key == 'action') {
-						export_csv_url.searchParams.set(key, 'availreport.view.csv');
-					}
-				}
-				var csv_url=export_csv_url.pathname.slice(1) + '?' + export_csv_url.searchParams.toString();
-				var export_button = document.getElementById("export_csv");
-				export_button.setAttribute("data-url", csv_url);
+        availreportPage.prototype = {
+            createCountersRefresh: function(timeout) {
+                if (this.refresh_counters) {
+                    clearTimeout(this.refresh_counters);
+                    this.refresh_counters = null;
+                }
 
-				this.setLoading();
+                return setTimeout(() => this.getFiltersCounters(), timeout);
+            },
+            getFiltersCounters: function() {
+                return $.post('zabbix.php', {
+                    action: 'availreport.view.refresh',
+                    filter_counters: 1
+                }).done((json) => {
+                    if (json.filter_counters) {
+                        this.filter.updateCounters(json.filter_counters);
+                    }
+                }).always(() => {
+                    if (this.refresh_interval > 0) {
+                        this.refresh_counters = this.createCountersRefresh(this.refresh_interval);
+                    }
+                });
+            },
+            getCurrentForm: function() {
+                return $('form[name=availreport_view]');
+            },
+            addMessages: function(messages) {
+                $('.wrapper main').before(messages);
+            },
+            removeMessages: function() {
+                $('.wrapper .msg-bad').remove();
+            },
+            refresh: function() {
+                // Update export_csv url according to what's in filter fields
+                const export_csv_url = new URL(this.refresh_url, 'http://example.com');
+                for(var key of export_csv_url.searchParams.keys()) {
+                    if (key == 'action') {
+                        export_csv_url.searchParams.set(key, 'availreport.view.csv');
+                    }
+                }
+                var csv_url=export_csv_url.pathname.slice(1) + '?' + export_csv_url.searchParams.toString();
+                var export_button = document.getElementById("export_csv");
+                export_button.setAttribute("data-url", csv_url);
 
-				this.deferred = $.getJSON(this.refresh_url);
+                this.setLoading();
 
-				return this.bindDataEvents(this.deferred);
-			},
-			setLoading: function() {
-				//this.getCurrentForm().addClass('is-loading is-loading-fadein delayed-15s');
-				$('div[id=reports_availreport_filter]').addClass('is-loading is-loading-fadein');
-			},
-			clearLoading: function() {
-				//this.getCurrentForm().removeClass('is-loading is-loading-fadein delayed-15s');
-				$('div[id=reports_availreport_filter]').removeClass('is-loading is-loading-fadein');
-			},
-			doRefresh: function(body) {
-				this.getCurrentForm().replaceWith(body);
-			},
-			bindDataEvents: function(deferred) {
-				var that = this;
+                this.deferred = $.getJSON(this.refresh_url);
 
-				deferred
-					.done(function(response) {
-						that.onDataDone.call(that, response);
-					})
-					.fail(function(jqXHR) {
-						that.onDataFail.call(that, jqXHR);
-					})
-					.always(this.onDataAlways.bind(this));
+                return this.bindDataEvents(this.deferred);
+            },
+            setLoading: function() {
+                //this.getCurrentForm().addClass('is-loading is-loading-fadein delayed-15s');
+                $('div[id=reports_availreport_filter]').addClass('is-loading is-loading-fadein');
+            },
+            clearLoading: function() {
+                //this.getCurrentForm().removeClass('is-loading is-loading-fadein delayed-15s');
+                $('div[id=reports_availreport_filter]').removeClass('is-loading is-loading-fadein');
+            },
+            doRefresh: function(body) {
+                this.getCurrentForm().replaceWith(body);
+            },
+            bindDataEvents: function(deferred) {
+                var that = this;
 
-				return deferred;
-			},
-			onDataDone: function(response) {
-				this.clearLoading();
-				this.removeMessages();
-				this.doRefresh(response.body);
+                deferred
+                    .done(function(response) {
+                        that.onDataDone.call(that, response);
+                    })
+                    .fail(function(jqXHR) {
+                        that.onDataFail.call(that, jqXHR);
+                    })
+                    .always(this.onDataAlways.bind(this));
 
-				if ('messages' in response) {
-					this.addMessages(response.messages);
-				}
-			},
-			onDataFail: function(jqXHR) {
-				// Ignore failures caused by page unload.
-				if (jqXHR.status == 0) {
-					return;
-				}
+                return deferred;
+            },
+            onDataDone: function(response) {
+                this.clearLoading();
+                this.removeMessages();
+                this.doRefresh(response.body);
 
-				this.clearLoading();
+                if ('messages' in response) {
+                    this.addMessages(response.messages);
+                }
+            },
+            onDataFail: function(jqXHR) {
+                // Ignore failures caused by page unload.
+                if (jqXHR.status == 0) {
+                    return;
+                }
 
-				var messages = $(jqXHR.responseText).find('.msg-global');
+                this.clearLoading();
 
-				if (messages.length) {
-					this.getCurrentForm().html(messages);
-				}
-				else {
-					this.getCurrentForm().html(jqXHR.responseText);
-				}
-			},
-			onDataAlways: function() {
-				if (this.running) {
-					this.deferred = null;
-					this.scheduleRefresh();
-				}
-			},
-			scheduleRefresh: function() {
-				this.unscheduleRefresh();
+                var messages = $(jqXHR.responseText).find('.msg-global');
 
-				if (this.refresh_interval > 0) {
-					this.timeout = setTimeout((function() {
-						this.timeout = null;
-						this.refresh();
-					}).bind(this), this.refresh_interval);
-				}
-			},
-			unscheduleRefresh: function() {
-				if (this.timeout !== null) {
-					clearTimeout(this.timeout);
-					this.timeout = null;
-				}
+                if (messages.length) {
+                    this.getCurrentForm().html(messages);
+                }
+                else {
+                    this.getCurrentForm().html(jqXHR.responseText);
+                }
+            },
+            onDataAlways: function() {
+                if (this.running) {
+                    this.deferred = null;
+                    this.scheduleRefresh();
+                }
+            },
+            scheduleRefresh: function() {
+                this.unscheduleRefresh();
 
-				if (this.deferred) {
-					this.deferred.abort();
-				}
-			},
-			start: function() {
-				this.running = true;
-				this.refresh();
-			}
-		};
+                if (this.refresh_interval > 0) {
+                    this.timeout = setTimeout((function() {
+                        this.timeout = null;
+                        this.refresh();
+                    }).bind(this), this.refresh_interval);
+                }
+            },
+            unscheduleRefresh: function() {
+                if (this.timeout !== null) {
+                    clearTimeout(this.timeout);
+                    this.timeout = null;
+                }
 
-		window.availreport_page = new availreportPage();
-		window.availreport_page.start();
+                if (this.deferred) {
+                    this.deferred.abort();
+                }
+            },
+            start: function() {
+                this.running = true;
+                this.refresh();
+            }
+        };
+
+        window.availreport_page = new availreportPage();
+        window.availreport_page.start();
 
         // Subscribe to timeselector.rangeupdate event
         jQuery.subscribe('timeselector.rangeupdate', function(e, data) {
@@ -190,9 +198,10 @@
 
                 window.availreport_page.refresh_url=url.pathname.slice(1) + '?' + url.searchParams.toString();
                 window.availreport_page.refresh();
+
+                // Save timeselector data
+                window.availreport_page.timeselector_data = data;
             }
         });
-	});
-
-
+    });
 </script>
